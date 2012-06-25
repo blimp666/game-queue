a6c21b2a9ac58f2fe838b13a3cf0d15b9db8d4ce
blimp666 <skyburg@mail.ru>
2012-06-25 16:41:44 +0400

    Version 0.1.0

--- spec/game-queue_spec.rb
+++ spec/game-queue_spec.rb
@@ -1,7 +1,24 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'redis'
 
 describe "GameQueue" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  before(:all) do
+    $redis =  Redis.new(:host => 'localhost', :port => 6379)
+    GameQueue.redis = $redis
+    GameQueue.queue_name = 'skyburg_main_queue_test'
+  end
+
+  before(:each) do
+    $redis.del('skyburg_main_queue_test')
+  end
+
+  it "push something" do
+    expect { GameQueue.push(:lalaee, {pizdets: true})}.to_not raise_error
+  end
+
+  it "pull right data after push" do
+    data  = {"cool" => "data", "something" => "else"}
+    GameQueue.push(:event, data)
+    GameQueue.pop.should == ["event", data]
   end
 end
