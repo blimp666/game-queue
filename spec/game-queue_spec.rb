92b8a3eab2a8f368eccb290b21e5aecf2b547810
V_M <nobody@nowhere.com>
2012-11-27 13:28:37 +0400

    async_push_with_delay specs and bundle update

--- spec/game-queue_spec.rb
+++ spec/game-queue_spec.rb
@@ -6,6 +6,7 @@ describe "GameQueue" do
     $redis =  Redis.new(:host => 'localhost', :port => 6379)
     GameQueue.redis = $redis
     GameQueue.queue_name = 'skyburg_main_queue_test'
+    @data  = {"cool" => "data", "something" => "else", "started_at" => Time.now}
   end
 
   before(:each) do
@@ -19,12 +20,11 @@ describe "GameQueue" do
   context "after push" do
 
     before(:each) do
-      @data  = {"cool" => "data", "something" => "else", "started_at" => Time.now}
       GameQueue.push(:event, @data)
     end
 
     it "should pull right data on pull" do
-      GameQueue.pop.should == ["event", @data]
+      GameQueue.pop.should == ["event", @data, 0]
     end
 
     it "should delete everything on clean!" do
@@ -32,6 +32,21 @@ describe "GameQueue" do
       GameQueue.clean!
       GameQueue.pop.should be_nil
     end
+  end
+
+  context "async_push_with_delay" do
+
+    before(:each) { GameQueue.async_push_with_delay(1, :event, @data) }
+
+    it "should not push nothing immediately" do
+      GameQueue.pop.should be_nil
+    end
+
+    it "should push message after delay" do
+      sleep 1.1
+      GameQueue.pop.should == ["event", @data, 0]
+    end
+
   end
 
 end
